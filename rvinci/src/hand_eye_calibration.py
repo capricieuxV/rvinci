09b73767d89db5ff1aea7b2686842fff0dbdb442
Shiyue(Vanessa) Wang <capricieuxyue@gmail.com>
2024-08-16 14:52:05 -0400

    move both in cp

--- rvinci/src/hand_eye_calibration.py
+++ rvinci/src/hand_eye_calibration.py
@@ -6,11 +6,11 @@ import rospy
 
 class DaVinciHandEyeCalibration:
     def __init__(self):
-        # Initialize CRTK and dVRK interfaces for PSM1
+        # Initialize CRTK and dVRK interfaces for PSM2
         self.ral_psm = crtk.ral('hand_eye_calibration')
         self.psm = dvrk.arm(self.ral_psm, 'PSM2')
         
-        # Initialize CRTK and dVRK interfaces for MTM1
+        # Initialize CRTK and dVRK interfaces for MTMR
         self.ral_mtm = crtk.ral('hand_eye_calibration')
         self.mtm = dvrk.arm(self.ral_mtm, 'MTMR')
 
@@ -37,34 +37,45 @@ class DaVinciHandEyeCalibration:
         self.calibration_data.append((psm_position, mtm_position))
         rospy.loginfo("Captured data point.")
 
-    def move_psm_to_position(self, position):
-        # Move PSM to a specified position in joint space
-        rospy.loginfo("Moving PSM to position... {}".format(position))
-        self.psm.move_jp(np.array(position)).wait()
-
-    def move_mtm_in_cartesian(self, dx=0.0, dy=0.0, dz=0.05, rotation_deg=0.0):
-        # Get the current position
-        rospy.loginfo("Moving MTM in cartesian space... dx: {}, dy: {}, dz: {}, rotation_deg: {}".format(dx, dy, dz, rotation_deg))
-        goal = self.mtm.setpoint_cp()
+    def move_psm_in_cartesian(self, dx=0.0, dy=0.0, dz=0.05, rotation_deg=0.0):
+        # Get the current position of PSM
+        rospy.loginfo("Moving PSM in Cartesian space... dx: {}, dy: {}, dz: {}, rotation_deg: {}".format(dx, dy, dz, rotation_deg))
+        psm_goal = self.psm.setpoint_cp()
         
-        # Move MTM in the cartesian space
-        goal.p[0] += dx
-        goal.p[1] += dy
-        goal.p[2] += dz
+        # Move PSM in Cartesian space
+        psm_goal.p[0] += dx
+        psm_goal.p[1] += dy
+        psm_goal.p[2] += dz
 
         # Rotate tool tip frame if needed
         if rotation_deg != 0.0:
-            goal.M.DoRotX(np.deg2rad(rotation_deg))
+            psm_goal.M.DoRotX(np.deg2rad(rotation_deg))
         
-        self.mtm.move_cp(goal).wait()
+        self.psm.move_cp(psm_goal).wait()
+
+    def move_mtm_in_cartesian(self, dx=0.0, dy=0.0, dz=0.05, rotation_deg=0.0):
+        # Get the current position of MTM
+        rospy.loginfo("Moving MTM in Cartesian space... dx: {}, dy: {}, dz: {}, rotation_deg: {}".format(dx, dy, dz, rotation_deg))
+        mtm_goal = self.mtm.setpoint_cp()
+        
+        # Move MTM in Cartesian space
+        mtm_goal.p[0] += dx
+        mtm_goal.p[1] += dy
+        mtm_goal.p[2] += dz
+
+        # Rotate tool tip frame if needed
+        if rotation_deg != 0.0:
+            mtm_goal.M.DoRotX(np.deg2rad(rotation_deg))
+        
+        self.mtm.move_cp(mtm_goal).wait()
 
     def run_calibration(self):
         for i in range(5):  # Adjust the number of iterations as needed
-            # Move PSM to a new position
-            self.move_psm_to_position([0.0, 0.0, 0.10 + i * 0.01, 0.0, 0.0, 0.0])
+            # Move PSM in Cartesian space
+            self.move_psm_in_cartesian(dx=0.01 * i, dz=0.01 * i, rotation_deg=5 * i)
             
-            # Move MTM in cartesian space
-            self.move_mtm_in_cartesian(dz=i * 0.01, rotation_deg=10 * i)
+            # Move MTM in Cartesian space
+            self.move_mtm_in_cartesian(dx=-0.01 * i, dz=0.01 * i, rotation_deg=-5 * i)
             
             # Capture the data at this configuration
             self.capture_data()
